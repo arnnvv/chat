81ebe63b4af521f69ddb1aaecd885349c57994d5
arnnvv <arnav.900@protonmail.com>
2024-07-31 06:46:11 +0530

    btrToast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,5 @@ import { redirect } from "next/navigation";
 export default async function Home(): Promise<JSX.Element> {
   const { user } = await validateRequest();
   if (!user) return redirect("login");
-  return <>HI</>;
+  return <>HEY</>;
 }
---
+++ src/app/test-toast/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function PAge(): JSX.Element {
+  return (
+    <Button
+      variant="outline"
+      onClick={(): string | number =>
+        toast("Event has been created", {
+          description: "MAA KI CHUT",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        })
+      }
+    >
+      Show Toast
+    </Button>
+  );
+}
