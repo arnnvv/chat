b0a8d667502399be18fe4e4736ba26b7fe4236ff
arnnvv <arnav.900@protonmail.com>
2025-09-11 13:34:57 +0530

    up

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@node-rs/argon2"],
   experimental: {
+    ppr: true,
     useCache: true,
     reactCompiler: true,
   },
